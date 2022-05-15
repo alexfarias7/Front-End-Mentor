3757849edc5bf7a44fdc9897b2fb136583b799d3
alexfarias7 <alexfarias44@gmail.com>
2022-05-15 04:29:16 -0300

    add project02

--- src/pages/Projects/project2/index.jsx
+++ src/pages/Projects/project2/index.jsx
@@ -1,30 +1,9 @@
-import "./styles.css";
-
-import Primary from "./Primary";
-
-import { useState } from "react";
-import Secondary from "./Secondary";
-
-const stages = [
-  { id: 1, name: "primary" },
-  { id: 2, name: "secondary" },
-];
-
 const Project2 = () => {
-  const [Stage, setStage] = useState(stages[0].name);
-
-  const changeStage = () => {
-    setStage(stages[1].name);
-  };
-
-  return (
-    <div className="Project">
-      <div className="general">
-        {Stage === "primary" && <Primary changeStage={changeStage} />}
-        {Stage === "secondary" && <Secondary />}
-      </div>
-    </div>
-  );
-};
-
+    return ( 
+        <div>
+            project 02 787777777777
+        </div>
+     );
+}
+ 
 export default Project2;
